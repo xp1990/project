c1526ff4b5475b92a2b7c07a1b009b52245cdf52
Edward <xp@xp-laptop.(none)>
2013-02-19 21:18:22 +0000

    made python more functional. added printing code. kind of works.

--- Pyhton/GoL.py
+++ Pyhton/GoL.py
@@ -6,7 +6,14 @@ right paradigm to use considering the other features Pyhton offers.
 4. Notice the With block! Similar to try/catch in Java, opens and closes
 streams"""
 
+"""None of the functions have side effects (beside conway(), which does
+printing), as emphasized by the functional programming style. Also, no function
+depends on some global state - hence they are all deterministic. This means that
+those functions always return the same output when called with a specific input.
+They are a mapping of input to output"""
+
 import pprint
+import pickle
 
 def initGrid():
     #list comprehension
@@ -25,6 +32,7 @@ def readFile(grid, filename):
         f.read(1)
         x = x + 1
         y = 1
+    return grid
 
 #POI: range gives a reading which is inclusive of the first argument but
 #exclusive of the second argument!!
@@ -35,14 +43,18 @@ def copyGhostCells(x):
     for i in range(dim + 2):
         x[0][i] = x[dim][i]
         x[dim + 1][i] = x[1][i]
+    return x
 
-def countGrid(x):
-    return [[(x[i - 1][j - 1] + x[i - 1][j] + x[i - 1][j + 1]
+def count(i, j):
+    return (x[i - 1][j - 1] + x[i - 1][j] + x[i - 1][j + 1]
      + x[i][j - 1] + x[i][j + 1] + x[i + 1][j - 1] +
      x[i + 1][j] + x[i + 1][j + 1])
-     for j in range(1, len(x[1]) - 1)] for i in range(1, len(x) - 1)]
 
-def change(i, j):
+def countGrid(x):
+    return [[count(i, j) for j in range(1, len(x[1]) - 1)] for i in range(1, len(x) - 1)]
+    #return [[1 for j in range(1, len(x[1]) - 1)] for i in range(1, len(x) - 1) if rules(count(i, j)) == 1]
+
+def change(count_grid, x, i, j):
     if i > 0 and i < dim + 1 and j > 0 and j < dim + 1:
         if count_grid[i - 1][j - 1] == 3 or (count_grid[i - 1][j - 1] == 2 and x[i][j] == 1):
             return 1
@@ -54,25 +66,34 @@ def change(i, j):
         return 0
 
 
-def evolve():
-    return [[(change(i, j)) for j in range(dim + 2)] for i in range(dim + 2)]
+def evolve(count_grid, x):
+    return [[(change(count_grid, x, i, j)) for j in range(dim + 2)] for i in range(dim + 2)]
 
 def sumlist(L):
     return sum(map(sum,L))
 
-dim = 512
+def outputFile(gen):
+    w = open("gen%i.ppm" % gen, 'w')
+    w.write("P1\n%i\n%i\n" % (dim, dim))
+    for i in range(1, dim):
+        for j in range(1, dim):
+            w.write(" " + str(new_grid[i][j]))
+        w.write("\n")
+
+dim = 2048
 x = initGrid()
-readFile(x, "512.dat")
+x = readFile(x, "2048.dat")
 print("Read file complete")
 #pprint.pprint(x)
 
-for gen in range(100):
-    copyGhostCells(x)
+for gen in range(10):
+    x = copyGhostCells(x)
     count_grid = countGrid(x)
-    new_grid = evolve()    
+    new_grid = evolve(count_grid, x)    
     if gen != 0:
         x = new_grid
     print(gen, "complete", sumlist(new_grid))
+    outputFile(gen)
 
 print(gen, sumlist(new_grid))
-
+ 
